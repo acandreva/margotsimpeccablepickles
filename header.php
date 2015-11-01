76ea74e4771d1a850cd132429e859b7bc06b6e31
Allison Logan <ali.cand@yahoo.com>
2015-11-01 12:31:12 -0500

    adds mobile menu and updates styles

--- header.php
+++ header.php
@@ -4,9 +4,11 @@
 <?php wp_head(); ?>
     <!-- Links to our Style.css file -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="http://margots.heatherlaude.com/wp-content/themes/margotstheme/js/main.js"></script>
-    <script type="text/javascript" src="http://margots.heatherlaude.com/wp-content/themes/margotstheme/js/shymenu.js"></script>
+    <script type="text/javascript" src="http://margots.heatherlaude.com/wp-content/themes/margotstheme/js/jquery.fatNav.js"></script>
 
     <!-- Typekit -->
     <script src="https://use.typekit.net/kxt8yad.js"></script>
@@ -15,28 +17,23 @@
 </head>
 <body>
 <!--Mobile Nav-->
-        <div class="shy-menu mobile-nav">
-            <a class="shy-menu-hamburger">
-                <span class="layer top"></span>
-                <span class="layer mid"></span>
-                <span class="layer btm"></span>
-            </a>
-            <div class="shy-menu-panel">
-                <ul>
-                    <?php wp_nav_menu( array(
-                        'theme_location' => 'mobile-menu',
-                        'container_class' => 'mobile_menu'
-                    )); ?>
-                </ul>
-            </div>
+    <div class="mobile-nav">
+        <div class="fat-nav">
+          <div class="fat-nav__wrapper">
+            <ul>
+                <?php wp_nav_menu( array(
+                    'theme_location' => 'mobile-menu',
+                    'container_class' => 'mobile_menu'
+                )); ?>
+            </ul>
+          </div>
         </div>
-        <div class="menu open">
-            <a class="hamburger">
-                <span class="layer top"></span>
-                <span class="layer mid"></span>
-                <span class="layer btm"></span>
+        <div class="mobile_logo">
+            <a href="http://margots.heatherlaude.com/" atl="margot's homepage">
+                <img src="http://margots.heatherlaude.com/wp-content/themes/margotstheme/images/margotslogo.png" />
             </a>
         </div>
+    </div>
 <!--Desktop Nav-->
     <header class="desktop-nav">
         <div class="container">
